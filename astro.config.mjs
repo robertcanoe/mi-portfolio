4097f6f4fea5f40918e8b9afac5c1f00cb084c45
robertcano <rcanoestevez@proton.me>
2025-04-14 13:33:04 +0200

    Add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://tudominio.com'
+
+  output: 'server',
+  adapter: vercel(),
+
 })
