6e8a7173257eab1647b34a6e93d86b771c2fd95f
robertcanoe <rcanoestevez@proton.me>
2026-03-24 13:29:50 +0100

    fix: update Vercel adapter import for compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
