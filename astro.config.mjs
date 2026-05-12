492130abcd79cdafc6d6d2753713391c38916006
robertcano <rcanoestevez@proton.me>
2026-05-12 21:18:59 +0200

    refactor: remove robotsTxt integration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-import robotsTxt from "astro-robots-txt"
 import vercel from '@astrojs/vercel'
 
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind()],
 
   output: 'server',
   adapter: vercel(),
