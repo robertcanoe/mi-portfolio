6119f9492db384430fbdaa98d9234b2d67481f6f
robertcano <rcanoestevez@proton.me>
2026-05-12 21:28:56 +0200

    chore: add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind"
 import vercel from '@astrojs/vercel'
 
 export default defineConfig({
+  site: "https://robertcanoe.dev",
   integrations: [tailwind()],
 
   output: 'server',
